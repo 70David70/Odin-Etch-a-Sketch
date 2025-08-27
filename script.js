8cedfbaff1d7d24c2794669fc5e59f822a6b979c
Daivd <drgdeved@gmail.com>
2025-08-28 02:50:03 +0300

    Write the logic of the js in pseudocode before starting to write code

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+    Logic:
+
+        Create an object that has all the brushes available to use
+            it should have the (the chosen brush. the chosen color. the brush size)
+        
+        make the grid
+            using math, Get the size of the dives that would be sufficient to fill drawing-box
+            make a function that creates divs with the size we just calculated
+            in css, make the divs be wrapped inside the drawing box
+
+        add event listeners
+            for each brush button that's pressed, Change the brush object accordingly
+
+        TO BE CONTINUED.......
+*/
